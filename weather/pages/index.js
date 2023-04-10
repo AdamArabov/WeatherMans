b30d087a37a61588fa3cd1657a3bc23071cf0b4f
AdamArabov <arabovadam@yahoo.com>
2023-04-10 14:39:58 -0400

    api connected

--- weather/pages/index.js
+++ weather/pages/index.js
@@ -1,8 +1,31 @@
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
+import axios from 'axios'
+import { useState } from 'react'
+import {BsSearch} from 'react-icons/bs'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return;
+
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState({})
+  const [loading, setLoading] = useState(false)
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+
+  function fetchWeather (e) {
+    e.preventDefault();
+    setLoading(true)
+    axios.get(url).then((response)=> {
+      setWeather(response.data)
+      console.log(response.data)
+    })
+    setCity('')
+    setLoading(false)
+  }
+  return (
+    <div>
+     <button onClick={fetchWeather}>Fetch</button>
+    </div>
+  )
 }
