799323fccdab1209d53c54d955202aff10ee2b74
AdamArabov <arabovadam@yahoo.com>
2023-04-10 15:09:48 -0400

    backgroung img

--- weather/pages/index.js
+++ weather/pages/index.js
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import axios from 'axios'
 import { useState } from 'react'
 import {BsSearch} from 'react-icons/bs'
+import Image from 'next/legacy/image'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,7 @@ export default function Home() {
   const [city, setCity] = useState('')
   const [weather, setWeather] = useState({})
   const [loading, setLoading] = useState(false)
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 
   function fetchWeather (e) {
     e.preventDefault();
@@ -25,7 +27,14 @@ export default function Home() {
   }
   return (
     <div>
-     <button onClick={fetchWeather}>Fetch</button>
+
+    <Image 
+    src = "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1213&q=80" alt = ""
+    layout='fill'
+    className='object-cover'
+    />
+
+    
     </div>
   )
 }
